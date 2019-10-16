3dbc16a6041f10925602527a8edcee3671f8aac9
Mokee.nmy <ningmuyu@live.com>
2019-10-16 20:06:56 +0800

    爬取chiphell图片的练习

---
+++ chiphell_channel.py
@@ -0,0 +1,132 @@
+'''
+@Description: In User Settings Edit
+@Author: your name
+@Date: 2019-10-16 17:23:03
+@LastEditTime: 2019-10-16 19:35:36
+@LastEditors: Please set LastEditors
+'''
+
+
+import urllib.request
+import os
+import ssl
+
+channel_url = 'https://www.chiphell.com/portal.php?mod=list&catid=102&page=1'
+
+#链接打开函数
+def url_open(channel_url):
+    #定义header
+    headers = {'User-Agent':'Mozilla/5.0 3578.98 Safari/537.36'}
+    #添加https验证忽略
+    context = ssl._create_unverified_context()
+    req = urllib.request.Request(url=channel_url, headers=headers)
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    return html
+
+#列表页页码
+def Channel_url(channel_url):
+    html = url_open(channel_url).decode('utf-8')
+    #列表页地址
+    channel_pages = []
+    
+    #获取总页码数
+    a = html.find('<span title="共 ')
+    b = html.find(' 页', a)
+    page_num = html[a+15:b]
+    page_num = int(page_num) + 1
+    
+    for i in range(1, page_num):
+        channel_urls = channel_url[:-1] + str(i)
+        channel_pages.append(channel_urls)
+    return channel_pages
+
+#获取列表页内的详情页地址，生成列表
+def Detail():
+    channel_pages = Channel_url(channel_url)
+    detail_page = []
+    for i in channel_pages:
+        html = url_open(i).decode('utf-8')        
+        a = html.find('"xs2"><a href="')
+        while a != -1:
+            b = html.find('" target', a)
+            if b != -1:
+                detail_href = html[a+15:b]
+                detail = 'https://www.chiphell.com/' + detail_href
+                detail_page.append(detail)
+            else:
+                b = a + 45
+            a = html.find('"xs2"><a href="', b)
+    with open('detail_url.txt', 'w', encoding='utf-8') as f:
+        for i in detail_page:
+            f.write(i)   
+
+'''#获取详情页的图片地址
+def img_src(detail_page):
+    detail_page = Detail()
+    for detail_url in detail_page:
+        html = url_open(detail_url).decode('utf-8')
+        imgs = []
+        a = html.find('zoomfile="')
+        #开始循环查找，如果能找到则 a!=-1
+        while a != -1:
+            b = html.find('.jpg', a)
+            if b != -1:
+                imgs.append(html[a+10:b+4])
+            else:
+                b = a + 60
+            #a的下一次查找从b开始
+            a = html.find('zoomfile="', b)
+        return imgs
+
+#将获取到的图片地址写入到文件夹
+def img_download(folder, imgs):
+    for i in imgs:
+        #通过分割图片地址获取图片名字
+        img_name = i.split('/')[-1]
+        print(img_name)
+        #打开生成一个图片
+        with open(img_name, 'wb') as f:
+            #打开图片地址
+            img = url_open(i)
+            #写入图片
+            f.write(img)
+
+#文件夹名字
+def folder():
+    detail_page = Detail()
+    for detail_url in detail_page:
+        html = url_open(detail_url).decode('utf-8')
+        a = html.find('<title>')
+        b = html.find(' - 摄影作品', a)
+        folder = html[a+7:b]
+        return folder
+
+#图片下载
+def Downloader(folder=folder()):
+    #创建文件夹
+    #os.mkdir(folder)
+    #打开文件夹
+    os.chdir(folder)
+    #执行图片下载函数
+    #Channel_url(channel_url)
+    detail_page = Detail()
+    for i in detail_page:
+        images = img_src(i)
+        img_download(folder, images)
+    #detail_page = Detail()
+    #for detail_url in detail_page:
+    #执行图片地址获取函数生成图片列表
+    
+
+    
+
+if __name__ == '__main__':
+    Downloader()
+    print('下载完成……')
+'''
+
+
+
+if __name__ == '__main__':
+    Detail()
---
+++ chiphell_detail.py
@@ -0,0 +1,79 @@
+'''
+@Description: In User Settings Edit
+@Author: your name
+@Date: 2019-10-16 16:02:53
+@LastEditTime: 2019-10-16 19:57:36
+@LastEditors: Please set LastEditors
+'''
+import urllib.request
+import os
+import ssl
+
+
+detail_url = input('请输入想要保存图片的详情页地址：')
+
+#定义链接的打开函数，防止https证书问题、header问题
+def url_open(url):
+    headers = {'User-Agent':'Mozilla/5.0 3578.98 Safari/537.36'}
+    #添加忽略https证书验证
+    context = ssl._create_unverified_context()
+    req = urllib.request.Request(url=url, headers=headers)
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    #print(url)
+    return html
+
+#通过循环方式将页面图片地址生成一个列表
+def img_src(detail_url):
+    html = url_open(detail_url).decode('utf-8')
+    imgs = []
+    a = html.find('zoomfile="')
+    #开始循环查找，如果能找到则 a!=-1
+    while a != -1:
+        b = html.find('.jpg', a)
+        if b != -1:
+            imgs.append(html[a+10:b+4])
+        else:
+            b = a + 60
+        #a的下一次查找从b开始
+        a = html.find('zoomfile="', b)
+    return imgs
+        #print(imgs)
+
+#将获取到的图片地址写入到文件夹
+def img_download(folder, imgs):
+    for i in imgs:
+        #通过分割图片地址获取图片名字
+        img_name = i.split('/')[-1]
+        print(img_name)
+        #打开生成一个图片
+        with open(img_name, 'wb') as f:
+            #打开图片地址
+            img = url_open(i)
+            #写入图片
+            f.write(img)
+
+#文件夹名字
+def folder():
+    html = url_open(detail_url).decode('utf-8')
+    a = html.find('<title>')
+    b = html.find(' - 摄影作品', a)
+    folder = html[a+7:b]
+    return folder
+
+#图片下载
+def Downloader(folder=folder()):
+    #创建文件夹
+    os.mkdir(folder)
+    #打开文件夹
+    os.chdir(folder)
+    #执行图片地址获取函数生成图片列表
+    images = img_src(detail_url)
+    #执行图片下载函数
+    #for i in images:
+    img_download(folder, images)
+
+if __name__ == '__main__':
+    Downloader()
+    print('下载完成……')
+
